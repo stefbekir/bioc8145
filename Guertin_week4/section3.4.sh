6df4758954fb2444158602475527de9793963e78
mjg54 <guertinmj@gmail.com>
2020-04-01 19:41:52 -0400

    Add files via upload

---
+++ Guertin_week4/section3.4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+idx=hg38
+spliceFile=gencode.v33.splice.txt
+processors=3
+
+for i in *PE1.fastq.gz
+do
+    name=$(echo $i | awk -F"_PE." '{print $1}')
+    echo $name
+    hisat2 -p ${processors} -x ${idx} --rna-strandness FR --known-splicesite-infile ${spliceFile} \
+        -1 ${i} -2 ${name}_PE2.fastq.gz | samtools view -bS - | samtools sort -n - -o $name.sorted.bam
+done
