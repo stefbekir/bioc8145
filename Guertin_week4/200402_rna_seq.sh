6eacf379d9ae5975f74d5d47527401a3886e0ae7
mjg54 <guertinmj@gmail.com>
2020-04-02 22:45:42 -0400

    Update 200402_rna_seq.sh

--- Guertin_week4/200402_rna_seq.sh
+++ Guertin_week4/200402_rna_seq.sh
@@ -2,7 +2,7 @@
 
 idx=hg38
 spliceFile=gencode.v33.splice.txt
-processors=3
+processors=10
 
 wget https://raw.githubusercontent.com/DaehwanKimLab/hisat2/master/hisat2_extract_splice_sites.py
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_33/gencode.v33.annotation.gtf.gz
@@ -14,16 +14,16 @@ hisat2-build hg38.fa ${idx}
 gunzip gencode.v33.annotation.gtf.gz
 python3 hisat2_extract_splice_sites.py gencode.v33.annotation.gtf > ${spliceFile}
 
-for i in A549_30min100nMdex_rep1*PE1.fastq.gz
+for i in *PE1.fastq.gz
 do
     name=$(echo $i | awk -F"_PE." '{print $1}')
     echo $name
     hisat2 -p ${processors} -x ${idx} --rna-strandness FR --known-splicesite-infile ${spliceFile} \
         -1 ${i} -2 ${name}_PE2.fastq.gz | samtools view -bS - | samtools sort -n - -o $name.sorted.bam
-    samtools fixmate -m $i - | samtools sort - | samtools markdup -rs - $name.rmPCRdup.bam
-    htseq-count -r pos -f bam --stranded=yes $i gencode.v33.annotation.gtf > $name.gene.counts.txt
-    samtools view -b -f 0x40 $i | samtools sort - -o ${name}.PE1.sorted
-    samtools view -b -f 0x80 $i | samtools sort - -o ${name}.PE2.sorted
+    samtools fixmate -m $name.sorted.bam  - | samtools sort - | samtools markdup -rs - $name.rmPCRdup.bam
+    htseq-count -r pos -f bam --stranded=yes $name.rmPCRdup.bam gencode.v33.annotation.gtf > $name.gene.counts.txt
+    samtools view -b -f 0x40 $name.rmPCRdup.bam | samtools sort - -o ${name}.PE1.sorted
+    samtools view -b -f 0x80 $name.rmPCRdup.bam | samtools sort - -o ${name}.PE2.sorted
     genomeCoverageBed -bg -strand - -split -ibam ${name}.PE1.sorted | sortBed -i - > $name.pe1.minus.bedGraph
     genomeCoverageBed -bg -strand + -split -ibam ${name}.PE1.sorted | sortBed -i - > $name.pe1.plus.bedGraph
     genomeCoverageBed -bg -strand + -split -ibam ${name}.PE2.sorted | sortBed -i - > $name.pe2.minus.bedGraph
