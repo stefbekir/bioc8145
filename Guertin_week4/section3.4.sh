358c56e68a38a66d8da70db96dc6d72dbab13fa6
mjg54 <guertinmj@gmail.com>
2021-06-11 11:07:00 -0400

    Update section3.4.sh

--- Guertin_week4/section3.4.sh
+++ Guertin_week4/section3.4.sh
@@ -6,7 +6,7 @@ processors=3
 
 for i in *PE1.fastq.gz
 do
-    name=$(echo $i | awk -F"_PE." '{print $1}')
+    name=$(echo $i | awk -F"_PE1." '{print $1}')
     echo $name
     hisat2 -p ${processors} -x ${idx} --rna-strandness FR --known-splicesite-infile ${spliceFile} \
         -1 ${i} -2 ${name}_PE2.fastq.gz | samtools view -bS - | samtools sort -n - -o $name.sorted.bam
